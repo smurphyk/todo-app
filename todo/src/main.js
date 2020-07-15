2995f9f6e70bdb455ac4facdef7ad00e7bf4ca55
Sean Murphy <smurphyk@DESKTOP-IFP8V84.localdomain>
2020-07-15 10:22:28 -0500

    updated main.js to incorporate material design elements

--- todo/src/main.js
+++ todo/src/main.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
-import { MdField } from 'vue-material/dist/components';
+import { MdField, MdButton, MdLIst, MdCheckbox, MdIcon, MdCard } from 'vue-material/dist/components';
 import 'vue-material/dist/vue-material.min.css';
+import 'vue-material/dist/theme/black-green-dark.css'
 
 Vue.use(MdField);
+Vue.use(MdButton);
+Vue.use(MdList);
+Vue.use(MdCheckbox);
+Vue.use(MdIcon);
+Vue.use(MdCard);
 
 Vue.config.productionTip = false
 
