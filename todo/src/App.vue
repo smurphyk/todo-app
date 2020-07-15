2602f3b728a45a935c005aed1955d3a1f70e11df
Sean Murphy <smurphyk@DESKTOP-IFP8V84.localdomain>
2020-07-15 10:18:43 -0500

    incorporated material design into app.vue

--- todo/src/App.vue
+++ todo/src/App.vue
@@ -1,49 +1,105 @@
 <template>
   <div>
-    <md-field>
-      <md-input v-model="currentTodo" @keydown.enter="addTodo()" placeholder="Add todo"></md-input>
+    <md-card class="main-card">
+      <md-card-header>
+        <div class="main-title">ToDo or not ToDo</div>
+        <div class="main-subtitle">The Perfect ToDo List</div>
+        </md-card-header>
+        <md-card-content>
+    <md-field :class="{'md-invalid': errorMessage}">
+      <md-input 
+        class="todo-input"
+        v-model="currentTodo" 
+        @keydown.enter="addTodo()" 
+        placeholder="Add todo">
+        </md-input>
+        <span class="error">Don't be lazy! Find something to do!</span>
       </md-field>
-    <ul class="todos">
-      <li v-for="todo in todos" :key="todo.id" @dblclick="editTodo(todo, true)" :class="{ completedTodo: todo.completed }">
-        {{ todo.label }} <input type="checkbox" @click="completeTodo(todo)"/><button @click="removeTodo(todo)">Remove</button>
-      </li>
-    </ul>
+    <md-list class="todos">
+      <draggable v-model="todos" group="list-items" :disabled="!dragAllowed" @start="drag=true" @end="drag=false">
+      <md-list-item 
+        v-for="todo in todos" 
+        v-if="todo.edit===false" 
+        :key="todo.id" 
+        @dblclick="editTodo(todo, true)" 
+        :md-ripple="false"
+        class="todo-item"
+        >
+        <div class="todo-div">
+          <md-checkbox v-model="todo.completed"></md-checkbox>
+          <div class="todo-label" :class="{ completeTodo: todo.completed }">{{ todo.label }}</div>
+        </div>
+        <div class="todo-actions">
+          <md-button class="edit-button" @click="editTodo(todo, true)">
+            <md-icon class="edit-img">Edit</md-icon>
+          </md-button>
+        </div>
+      </md-list-item>
+      <md-list-item v-else>
+        <md-field>
+          <md-input
+            class="edit-todo"
+            v-model="todo.label"
+            v-focus
+            @keydown.enter="editTodo(todo, false)"
+            @focusout="editTodo(todo, false)">
+          </md-input>
+        </md-field>
+      </md-list-item>
+      </draggable>
+    </md-list>
+  </md-card-content>
+  </md-card>
   </div>
 </template>
 
 <script>
+import draggable from 'vuedraggable'
 export default {
+  components: {
+    draggable
+  },
   data() {
     return {
       todos: [],
-      currentTodo: ''
+      currentTodo: '',
+      errorMessage: false,
+      dragAllowed: true
     };
   },
   methods: {
     addTodo() {
-      this.todos.push({
-        id: this.todos.length, 
-        label: this.currentTodo, 
-        completed: false,
-        edit: false
-      });
+      if (this.currentTodo.length === 0){
+        this.errorMessage = true;
+        }
+        else {
+          this.errorMessage = false;
+          this.todos.push({
+            id: this.todos.length, 
+            label: this.currentTodo, 
+            completed: false,
+            edit: false
+          });
+        }
       this.currentTodo = '';
     },
+
+    removeTodo(todo) {
+      this.errorMessage = false;
+      todo.edit = false;
+      var index = this.todos.indexOf(todo);
+      this.todos.splice(index, 1);
+    },
+
     editTodo(todo, toggle){
+      this.errorMessage = false;
       if (toggle){
         todo.edit = true;
-        todo.completed = false;
+        this.dragAllowed = false;
       }
       else {
         todo.edit = false;
-      }
-    },
-    completeTodo(todo) {
-      if (event.target.checked) {
-        todo.completed = true
-      }
-      else {
-        todo.completed = false;
+        this.dragAllowed = true;
       }
     }
   },
